e2fa3517e1f7f5f340352946dc5887fdb5f3cb69
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-12-14 17:42:43 +0900

    fix: .kt 파일 R class import 삭제

--- presentation/src/main/java/com/aga/presentation/MainActivity.kt
+++ presentation/src/main/java/com/aga/presentation/MainActivity.kt
@@ -3,7 +3,6 @@ package com.aga.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
-import com.steelzoo.presentation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- presentation/src/main/java/com/aga/presentation/StatisticsFragment.kt
+++ presentation/src/main/java/com/aga/presentation/StatisticsFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.aga.presentation.statistics.BottomSheetStatisticsFilter
-import com.steelzoo.presentation.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
