45dfed029dd2ce78482b3ada586ebd56decbac07
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-12-14 17:33:20 +0900

    fix: .kt 파일 패키지명 변경 후 R class 다시 import

--- presentation/src/main/java/com/aga/presentation/MainActivity.kt
+++ presentation/src/main/java/com/aga/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aga.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import com.steelzoo.presentation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
