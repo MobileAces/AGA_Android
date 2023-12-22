dda0ee95199ae6c3509f83cb05753ba2694656a2
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-12-23 01:17:55 +0900

    feat: 로그인 및 자동 로그인 기능 추가

--- presentation/src/main/java/com/aga/presentation/login/LoginFragment.kt
+++ presentation/src/main/java/com/aga/presentation/login/LoginFragment.kt
@@ -1,19 +1,25 @@
 package com.aga.presentation.login
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.viewModels
+import com.aga.domain.model.User
 import com.aga.presentation.LoginActivity
 import com.aga.presentation.MainActivity
 import com.aga.presentation.R
 import com.aga.presentation.base.BaseFragment
+import com.aga.presentation.base.Constants.LOGIN_TO_JOIN
+import com.aga.presentation.base.Constants.PREF_AUTO_LOGIN
+import com.aga.presentation.base.Constants.PREF_USER_ID
+import com.aga.presentation.base.PrefManager
 import com.aga.presentation.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.fragment.app.viewModels
 
 
 @AndroidEntryPoint
@@ -23,17 +29,84 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(
 
     private val viewModel: LoginViewModel by viewModels()
     private lateinit var activity: LoginActivity
+    private var autoLogin = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity = _activity as LoginActivity
+
+        //뒤로가기 2번 눌러 종료
+        activity.onBackPressedDispatcher.addCallback(viewLifecycleOwner, activity.onBackPressedCallback)
+
+        registerListener()
+        registerObserver()
+
+    }
+
+    private fun registerListener(){
+        @SuppressLint("ResourceType")
+        val vibration = AnimationUtils.loadAnimation(requireContext(), R.animator.ani_vibratation)
 
         binding.btnLogin.setOnClickListener {
             val intent = Intent(activity, MainActivity::class.java)
             startActivity(intent)
         }
 
+        binding.btnLogin.setOnClickListener {
+            val id = binding.etId.editText!!.text.toString().trim()
+            val pw = binding.etPw.editText!!.text.toString().trim()
+
+            if (id.isBlank() || pw.isBlank()){
+                showToast("아이디 비밀번호를 모두 입력하세요")
+                binding.llInput.startAnimation(vibration)
+            }else{
+                viewModel.login(User(id, pw, "", ""))
+            }
+        }
+
+        binding.cbAutoLogin.setOnCheckedChangeListener { compoundButton, b ->
+            compoundButton.startAnimation(vibration)
+            autoLogin = b
+        }
+
+        binding.tvJoin.setOnClickListener {
+            activity.navigate(LOGIN_TO_JOIN)
+        }
+
+        binding.etId.editText!!.setOnKeyListener { view, i, keyEvent ->
+            if ((keyEvent.action == KeyEvent.ACTION_DOWN) && (i == KeyEvent.KEYCODE_ENTER)) {
+                hideKeyboard()
+                true
+            }else
+                false
+        }
+
+        binding.etPw.editText!!.setOnKeyListener { view, i, keyEvent ->
+            if ((keyEvent.action == KeyEvent.ACTION_DOWN) && (i == KeyEvent.KEYCODE_ENTER)) {
+                hideKeyboard()
+                true
+            }else
+                false
+        }
     }
 
+    private fun registerObserver(){
+        viewModel.userInfo.observe(viewLifecycleOwner){
+            PrefManager.write(PREF_USER_ID, it.id)
+        }
+
+        viewModel.loginResult.observe(viewLifecycleOwner){
+            showToast(it)
+            if(it == LoginViewModel.LOGIN_SUCCESS){
+                PrefManager.write(PREF_AUTO_LOGIN, autoLogin)
+
+                Handler().postDelayed({
+                    val intent = Intent(activity, MainActivity::class.java)
+                    startActivity(intent)
+                    activity.finish()
+                }, 500)
+            }
+        }
+    }
 
 }
--- presentation/src/main/java/com/aga/presentation/login/LoginViewModel.kt
+++ presentation/src/main/java/com/aga/presentation/login/LoginViewModel.kt
@@ -1,7 +1,54 @@
 package com.aga.presentation.login
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aga.domain.model.User
+import com.aga.domain.usecase.user.LoginUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+private const val TAG = "LoginViewModel_AGA"
 
 @HiltViewModel
-class LoginViewModel {
+class LoginViewModel @Inject constructor(
+    private val loginUseCase: LoginUseCase
+) : ViewModel() {
+
+    private val _loginResult = MutableLiveData<String>()
+    val loginResult: LiveData<String>
+        get() = _loginResult
+
+    private val _userInfo = MutableLiveData<User>()
+    val userInfo : LiveData<User>
+        get() = _userInfo
+
+    fun login(user: User){
+        viewModelScope.launch {
+            try {
+                val response = loginUseCase.invoke(user)
+
+                if (response.id == WRONG_ACCOUNT_INFO)
+                    _loginResult.value = LOGIN_FAIL
+                else {
+                    _userInfo.value = response
+                    _loginResult.value = LOGIN_SUCCESS
+                }
+            }catch (e: Exception){
+                Log.d(TAG, "login: ${e.message}")
+                _loginResult.value = NET_ERR
+            }
+        }
+    }
+
+    companion object{
+        const val LOGIN_SUCCESS = "환영합니다."
+        const val LOGIN_FAIL = "계정 정보가 잘못 되었습니다."
+        const val NET_ERR = "네트워크에 문제가 있습니다."
+
+        const val WRONG_ACCOUNT_INFO = "NOT FOUND"
+    }
 }
