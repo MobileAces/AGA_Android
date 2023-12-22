5514a6bcb73d36336fdc68a750d1f173f0916551
정철주 <cjfwn96@gmail.com>
2023-12-22 19:30:58 +0900

    chore: 각 모듈 자바 버전 변경 1.8 -> 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.awesomegroupalarm"
+    namespace = "com.aga.app"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.awesomegroupalarm"
+        applicationId = "com.aga.app"
         minSdk = 26
         targetSdk = 33
         versionCode = 1
@@ -29,11 +29,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
