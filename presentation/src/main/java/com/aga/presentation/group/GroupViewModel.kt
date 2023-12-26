51636fcd46e266a423daf5cd946db12fbaf1cf7b
정철주 <cjfwn96@gmail.com>
2023-12-27 00:32:43 +0900

    create: GroupViewModel

---
+++ presentation/src/main/java/com/aga/presentation/group/GroupViewModel.kt
@@ -0,0 +1,11 @@
+package com.aga.presentation.group
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GroupViewModel @Inject constructor(
+    
+) {
+
+}
