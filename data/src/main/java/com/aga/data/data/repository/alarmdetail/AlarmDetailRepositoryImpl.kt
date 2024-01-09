f00e257665c420f9b4a0b20639f1a46c2cb74853
정철주 <cjfwn96@gmail.com>
2024-01-10 00:11:59 +0900

    create: 알람 디테일 레포지토리 생성 및 구현

---
+++ data/src/main/java/com/aga/data/data/repository/alarmdetail/AlarmDetailRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.aga.data.data.repository.alarmdetail
+
+import com.aga.data.data.model.mapper.toAlarmDetail
+import com.aga.data.data.model.mapper.toAlarmDetailModifyRequest
+import com.aga.data.data.model.mapper.toAlarmDetailRequest
+import com.aga.data.data.repository.alarmdetail.remote.AlarmDetailRemoteDataSource
+import com.aga.domain.model.AlarmDetail
+import com.aga.domain.repository.AlarmDetailRepository
+import javax.inject.Inject
+
+class AlarmDetailRepositoryImpl @Inject constructor(
+    private val alarmDetailRemoteDataSource: AlarmDetailRemoteDataSource
+) : AlarmDetailRepository {
+    override suspend fun createNewPersonalAlarm(alarmDetail: AlarmDetail): Result<AlarmDetail> {
+        return alarmDetailRemoteDataSource.createNewPersonalAlarm(
+            alarmDetail.toAlarmDetailRequest()
+        ).map {
+            it.toAlarmDetail()
+        }
+    }
+
+    override suspend fun modifyPersonalAlarm(alarmDetail: AlarmDetail): Result<AlarmDetail> {
+        return alarmDetailRemoteDataSource.modifyPersonalAlarm(
+            alarmDetail.toAlarmDetailModifyRequest()
+        ).map {
+            it.toAlarmDetail()
+        }
+    }
+}
