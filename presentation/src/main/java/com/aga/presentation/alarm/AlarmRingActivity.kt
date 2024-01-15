37009527d8cf63e3614061c399d430e328587539
ghddbwns9808 <ghddbwns9808@gmail.com>
2024-01-15 22:34:19 +0900

    feat: 알람 울리는 액티비티 추가

---
+++ presentation/src/main/java/com/aga/presentation/alarm/AlarmRingActivity.kt
@@ -0,0 +1,12 @@
+package com.aga.presentation.alarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aga.presentation.R
+
+class AlarmRingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_ring)
+    }
+}
