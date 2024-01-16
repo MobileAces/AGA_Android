1398a813fc28663e8717d1b1887b2b25c69f3609
정철주 <cjfwn96@gmail.com>
2024-01-16 20:57:05 +0900

    feat: 데이터베이스, 알람 디테일 DAO Hilt 모듈 추가

--- app/src/main/java/com/aga/app/module/LocalModule.kt
+++ app/src/main/java/com/aga/app/module/LocalModule.kt
@@ -1,11 +1,38 @@
 package com.aga.app.module
 
+import android.content.Context
+import androidx.room.Room
+import com.aga.data.data.AGADatabase
+import com.aga.data.data.dao.AlarmDetailDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object LocalModule {
+
+    @Provides
+    @Singleton
+    fun providesAppDatabase(
+        @ApplicationContext context: Context
+    ): AGADatabase {
+        return Room.databaseBuilder(
+            context,
+            AGADatabase::class.java,
+            "exchange_rate_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesAlarmDetailDao(
+        agaDatabase: AGADatabase
+    ): AlarmDetailDao {
+        return agaDatabase.alarmDetailDao()
+    }
 
 }
